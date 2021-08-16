d081e8389304ba7d47efac9ef73d3ba3d6c5d313
trbl <trbl@her.st>
2021-08-16 21:33:58 +0200

    close context menu on click, add DEBUG entry to context menu to remove localstorage-id and refresh page

--- frontend/js/game-screen.js
+++ frontend/js/game-screen.js
@@ -1,16 +1,31 @@
-function DevelopProductModal() 
+function DevelopProductModal()
 {
+    const contextMenu = document.getElementById("context-menu");
     document.getElementById("develop-product").style.display = "block"
+    contextMenu.classList.remove("visible")
 }
 function ResearchScreenModal() 
 {
+    const contextMenu = document.getElementById("context-menu");
     document.getElementById("research").style.display = "block"
+    contextMenu.classList.remove("visible")
 }
 function MarketAnalysisModal() 
 {
+    const contextMenu = document.getElementById("context-menu");
     document.getElementById("market-analysis").style.display = "block"
+    contextMenu.classList.remove("visible")
 }
 function AdvertisingModal() 
 {
+    const contextMenu = document.getElementById("context-menu");
     document.getElementById("advertising-campaign").style.display = "block"
+    contextMenu.classList.remove("visible")
+}
+function ClearLocalStorage()
+{
+    const contextMenu = document.getElementById("context-menu");
+    localStorage.removeItem("id")
+    contextMenu.classList.remove("visible")
+    window.location.reload()
 }
