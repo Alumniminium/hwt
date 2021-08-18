795f87e107c1b094b8e85ee8f887e4015764c887
trbl <trbl@her.st>
2021-08-18 19:52:59 +0200

    doing nekos work

--- frontend/js/Api/update.js
+++ frontend/js/Api/update.js
@@ -10,23 +10,32 @@ function update()
     
     });  
 }
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
 function Researches(){
   researchableList =document.getElementById("researchable")
   researchedList =document.getElementById("researchable")
-
+  removeElementsByClass("researched-modal-item")
+  removeElementsByClass("researchable-modal-item")
   fetch("http://localhost/api/research/?playerId=" + id)
   .then(res => res.json())
   .then(data => {
     data.forEach(research => {
       if(research.price == 0)
       {
-        var researched = document.createElement('div');  
+        var researched = document.createElement('div');
+        researched.className = "researched-modal-item"
         researched.textContent = research.name;    
         researched.style.backgroundColor = "#252";
         researchedList.appendChild(researched);   
       }
       else{
-        var researchable = document.createElement('div');  
+        var researchable = document.createElement('div');
+        researchable.className = "researchable-modal-item"
         researchable.textContent = research.name;    
         researchable.style.backgroundColor = "#213";
         researchable.addEventListener("click", function() {
