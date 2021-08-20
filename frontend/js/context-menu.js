10ba55ab97dc6e8952b611662bac687e866061c1
NekoSina <nekosalad@protonmail.com>
2021-08-20 19:21:38 +0430

    escape closes modals. fixed animation. fixed right click. fixed centered modals

--- frontend/js/context-menu.js
+++ frontend/js/context-menu.js
@@ -2,7 +2,24 @@ window.addEventListener("load", function ()
 {
     console.log("Context Menu setup starting...")
     const contextMenu = document.getElementById("context-menu");
-
+    document.getElementById("game-container").addEventListener("contextmenu",(event) =>{
+        if(event.target.id == "game-container")
+        {
+            if(contextMenu.style.display != "block")
+            {
+                if(isAnyModalOpen())
+                 CloseAllModals()
+                 else{
+                     contextMenu.style.display = "block"
+                     contextMenu.style.top = "" + event.clientY + "px"
+                     contextMenu.style.left = "" + event.clientX + "px"
+                     console.log(contextMenu.style.left,contextMenu.style.top)
+                 }
+            }
+            else
+                contextMenu.style.display = "none"
+        }
+    })
     document.getElementById("game-container").addEventListener("click",(event) =>{
         if(event.target.id == "game-container")
         {
--- frontend/js/game-screen.js
+++ frontend/js/game-screen.js
@@ -4,7 +4,18 @@ let timer = null
 window.addEventListener("load", function () {
     console.log("game-screen.js loading...")
     contextMenu = document.getElementById("context-menu");
-
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        var isEscape = false;
+        if ("key" in evt) {
+            isEscape = (evt.key === "Escape" || evt.key === "Esc");
+        } else {
+            isEscape = (evt.keyCode === 27);
+        }
+        if (isEscape) {
+            CloseAllModals();
+        }
+    };
     if (id == null || id == -1) {
         console.log("no id in localstorage, going to index.html")
         window.location.replace('index.html')
@@ -15,29 +26,58 @@ window.addEventListener("load", function () {
         timer = setInterval(ApiUpdate, 1000);
     }
 })
+window.addEventListener("keypress")
+function OpenModal(modalname){
+    modal = document.getElementById(modalname)
+    modal.style.display = "block"
+    gamecontainer = document.getElementById("game-container")
+    modal.style.top ="" + gamecontainer.style.left + gamecontainer.offsetHeight / 3 +"px"
+    modal.style.left ="" + gamecontainer.style.top + gamecontainer.offsetWidth / 2 - modal.offsetWidth / 2 +"px"
+    switch (modalname) {
+        case "research":
+            Researches()
+            break;
+        case "develop-product":
 
-function DevelopProductModal() {
-    document.getElementById("develop-product").style.display = "block"
+            break;
+        case "market-analysis":
+
+            break;
+        case "advertising-campaign":
+
+            break;
+        default:
+            break;
+    }
     contextMenu.style.display = "none"
 }
-function ResearchScreenModal() {
-    document.getElementById("research").style.display = "block"
-    Researches()
-    contextMenu.style.display = "none"}
-function MarketAnalysisModal() {
-    document.getElementById("market-analysis").style.display = "block"
-    contextMenu.style.display = "none"
-}
-function AdvertisingModal() {
-    document.getElementById("advertising-campaign").style.display = "block"
-    contextMenu.style.display = "none"
-}
+// function DevelopProductModal() {
+//     document.getElementById("develop-product").style.display = "block"
+//     contextMenu.style.display = "none"
+// }
+// function ResearchScreenModal() {
+//     modal = document.getElementById("research")
+//     modal.style.display = "block"
+//     modal.style.top ="" + document.body.offsetWidth / 2 +"px"
+//     modal.style.left ="" + document.body.offsetHeight / 2 +"px"
+//     Researches()
+//     contextMenu.style.display = "none"}
+// function MarketAnalysisModal() {
+//     modal = document.getElementById("market-analysis")
+//     modal.style.display = "block"
+//     modal.style.top = document.getElementById("game-container").style.top / 2
+//     modal.style.top = document.getElementById("game-container").style.left / 2
+//     contextMenu.style.display = "none"
+// }
+// function AdvertisingModal() {
+//     document.getElementById("advertising-campaign").style.display = "block"
+//     contextMenu.style.display = "none"
+// }
 function CloseAllModals() {
     document.getElementById("advertising-campaign").style.display = "none"
     document.getElementById("market-analysis").style.display = "none"
     document.getElementById("research").style.display = "none"
     document.getElementById("develop-product").style.display = "none"
-
 }
 function ClearLocalStorage() {
     localStorage.removeItem("id")
