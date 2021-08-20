faf643dda7876fb8bf672ef0e094782572dfe1d8
trbl <trbl@her.st>
2021-08-20 20:04:14 +0200

    code reduction

--- frontend/js/game-screen.js
+++ frontend/js/game-screen.js
@@ -4,29 +4,26 @@ let timer = null
 window.addEventListener("load", function () {
     console.log("game-screen.js loading...")
     contextMenu = document.getElementById("context-menu");
-    document.onkeydown = function(evt) {
-        evt = evt || window.event;
-            if (evt.key === "Escape" || evt.key === "Esc" || evt.keyCode === 27)
-                CloseAllModals();
+    document.onkeydown = function (evt) {
+        if (evt.key === "Escape" || evt.key === "Esc")
+            CloseAllModals();
     };
     if (id == null || id == -1) {
         console.log("no id in localstorage, going to index.html")
         window.location.replace('index.html')
     }
-    else 
-    {       
+    else {
         console.log("gameId = " + id + ", starting update timer...")
         timer = setInterval(ApiUpdate, 1000);
     }
 })
 
-function OpenModal(modalname) 
-{
+function OpenModal(modalname) {
     gamecontainer = document.getElementById("game-container")
     modal = document.getElementById(modalname)
     modal.style.display = "block"
-    modal.style.top ="" + gamecontainer.style.left + gamecontainer.offsetHeight / 3 +"px"
-    modal.style.left ="" + gamecontainer.style.top + gamecontainer.offsetWidth / 2 - modal.offsetWidth / 2 +"px"
+    modal.style.top = "" + gamecontainer.style.left + gamecontainer.offsetHeight / 3 + "px"
+    modal.style.left = "" + gamecontainer.style.top + gamecontainer.offsetWidth / 2 - modal.offsetWidth / 2 + "px"
     switch (modalname) {
         case "research":
             Researches()
